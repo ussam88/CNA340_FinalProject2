939f571e3a3548f9e75f363f636a3e4dc153e46c
Semion Kalinin <50159960+1kalisem1@users.noreply.github.com>
2019-06-18 17:17:48 -0700

    commit 6/18/19

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # This creates a basic eCommerce website using Flask and SQLite
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 340 Spring 2019
-
 from flask import *
 import sqlite3, hashlib, os
 from werkzeug.utils import secure_filename
-
 app = Flask(__name__)
 app.secret_key = 'cna340'
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 def get_login_details():
     with sqlite3.connect('database.db') as conn:
         cur = conn.cursor()
@@ -27,7 +24,6 @@ def get_login_details():
             no_of_items = cur.fetchone()[0]
     conn.close()
     return (logged_in, first_name, no_of_items)
-
 @app.route("/")
 def root():
     logged_in, first_name, no_of_items = get_login_details()
@@ -36,16 +32,15 @@ def root():
         # Show last product added
         cur.execute('SELECT productId, name, price, description, image, stock FROM products ORDER BY productId DESC LIMIT 1 ')
         # Show all items
-        cur.execute('SELECT productId, name, price, description, image, stock FROM products LIMIT 3')
+        cur.execute('SELECT productId, name, price, description, image, stock FROM products UNLIMIT ')
         item_data = cur.fetchall()
         # Show an error instead of the categories
-        category_data = [(-1)]
+        category_data = [(-1,"Error")]
         # Show all categories
         cur.execute('SELECT categoryId, name FROM categories')
         category_data = cur.fetchall()
     item_data = parse(item_data)
     return render_template('home.html', itemData=item_data, loggedIn=logged_in, firstName=first_name, noOfItems=no_of_items, categoryData=category_data)
-
 @app.route("/add")
 def admin():
     with sqlite3.connect('database.db') as conn:
@@ -54,7 +49,6 @@ def admin():
         categories = cur.fetchall()
     conn.close()
     return render_template('add.html', categories=categories)
-
 @app.route("/addItem", methods=["GET", "POST"])
 def addItem():
     if request.method == "POST":
@@ -63,7 +57,6 @@ def addItem():
         description = request.form['description']
         stock = int(request.form['stock'])
         categoryId = int(request.form['category'])
-
         #Upload image
         image = request.files['image']
         if image and allowed_file(image.filename):
@@ -82,7 +75,6 @@ def addItem():
         conn.close()
         print(msg)
         return redirect(url_for('root'))
-
 @app.route("/displayCategory")
 def displayCategory():
     logged_in, first_name, no_of_items = get_login_details()
@@ -97,15 +89,12 @@ def displayCategory():
     data = parse(data)
     return render_template('displayCategory.html', data=data, loggedIn=logged_in, firstName=first_name,
                            noOfItems=no_of_items, categoryName=category_name)
-
-
 @app.route("/account/profile")
 def profile_home():
     if 'email' not in session:
         return redirect(url_for('root'))
     logged_in, first_name, no_of_items = get_login_details()
     return render_template("profileHome.html", loggedIn=logged_in, firstName=first_name, noOfItems=no_of_items)
-
 @app.route("/account/profile/edit")
 def edit_profile():
     if 'email' not in session:
@@ -117,7 +106,6 @@ def edit_profile():
         profile_data = cur.fetchone()
     conn.close()
     return render_template("editProfile.html", profileData=profile_data, loggedIn=logged_in, firstName=first_name, noOfItems=no_of_items)
-
 @app.route("/account/profile/changePassword", methods=["GET", "POST"])
 def change_password():
     if 'email' not in session:
@@ -146,7 +134,6 @@ def change_password():
         return render_template("changePassword.html", msg=msg)
     else:
         return render_template("changePassword.html")
-
 @app.route("/updateProfile", methods=["GET", "POST"])
 def update_profile():
     if request.method == 'POST':
@@ -164,7 +151,6 @@ def update_profile():
                 try:
                     cur = con.cursor()
                     cur.execute('UPDATE users SET firstName = ?, lastName = ?, address1 = ?, address2 = ?, zipcode = ?, city = ?, state = ?, country = ?, phone = ? WHERE email = ?', (first_name, last_name, address1, address2, zipcode, city, state, country, phone, email))
-
                     con.commit()
                     msg = "Saved Successfully"
                 except:
@@ -172,15 +158,13 @@ def update_profile():
                     msg = "Error occured"
         con.close()
         return redirect(url_for('edit_profile'))
-
 @app.route("/loginForm")
 def login_form():
     # Uncomment to enable logging in and registration
-    if 'email' in session:
+    #if 'email' in session:
         return redirect(url_for('root'))
-    else:
-        return render_template('login.html', error='')
-
+    #else:
+    #    return render_template('login.html', error='')
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -192,7 +176,6 @@ def login():
         else:
             error = 'Invalid UserId / Password'
             return render_template('login.html', error=error)
-
 @app.route("/productDescription")
 def product_description():
     logged_in, first_name, no_of_items = get_login_details()
@@ -205,7 +188,6 @@ def product_description():
     conn.close()
     return render_template("productDescription.html", data=productData, loggedIn=logged_in, firstName=first_name,
                            noOfItems=no_of_items)
-
 @app.route("/addToCart")
 def add_to_cart():
     if 'email' not in session:
@@ -225,7 +207,6 @@ def add_to_cart():
                 msg = "Error occured"
         conn.close()
         return redirect(url_for('root'))
-
 @app.route("/cart")
 def cart():
     if 'email' not in session:
@@ -242,7 +223,6 @@ def cart():
     for row in products:
         total_price += row[2]
     return render_template("cart.html", products = products, totalPrice=total_price, loggedIn=logged_in, firstName=first_name, noOfItems=no_of_items)
-
 @app.route("/removeFromCart")
 def remove_from_cart():
     if 'email' not in session:
@@ -262,12 +242,10 @@ def remove_from_cart():
             msg = "error occured"
     conn.close()
     return redirect(url_for('root'))
-
 @app.route("/logout")
 def logout():
     session.pop('email', None)
     return redirect(url_for('root'))
-
 def is_valid(email, password):
     con = sqlite3.connect('database.db')
     cur = con.cursor()
@@ -277,15 +255,12 @@ def is_valid(email, password):
         if row[0] == email and row[1] == hashlib.md5(password.encode()).hexdigest():
             return True
     return False
-
-
 @app.route("/checkout", methods=['GET','POST'])
 def payment():
     if 'email' not in session:
         return redirect(url_for('login_form'))
     logged_in, first_name, no_of_items = get_login_details()
     email = session['email']
-
     with sqlite3.connect('database.db') as conn:
         cur = conn.cursor()
         cur.execute("SELECT userId FROM users WHERE email = '" + email + "'")
@@ -299,11 +274,7 @@ def payment():
         cur.execute("INSERT INTO Orders (userId, productId) VALUES (?, ?)", (user_id, row[0]))
     cur.execute("DELETE FROM kart WHERE userId = " + str(user_id))
     conn.commit()
-
-
-
     return render_template("checkout.html", products = products, totalPrice=total_price, loggedIn=logged_in, firstName=first_name, noOfItems=no_of_items)
-
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -319,29 +290,23 @@ def register():
         state = request.form['state']
         country = request.form['country']
         phone = request.form['phone']
-
         with sqlite3.connect('database.db') as con:
             try:
                 cur = con.cursor()
                 cur.execute('INSERT INTO users (password, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (hashlib.md5(password.encode()).hexdigest(), email, first_name, last_name, address1, address2, zipcode, city, state, country, phone))
-
                 con.commit()
-
                 msg = "Registered Successfully"
             except:
                 con.rollback()
                 msg = "Error occured"
         con.close()
         return render_template("login.html", error=msg)
-
 @app.route("/registrationForm")
 def registration_form():
     return render_template("register.html")
-
 def allowed_file(filename):
     return '.' in filename and \
             filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
 def parse(data):
     ans = []
     i = 0
@@ -354,6 +319,5 @@ def parse(data):
             i += 1
         ans.append(curr)
     return ans
-
 if __name__ == '__main__':
     app.run(debug=True)
